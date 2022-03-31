10db55df1f0b1c2a4a1b6ce3043786c68bee7213
Daniyar <daniyarmurza@gmail.com>
2022-03-31 16:09:02 -0500

    fixed2

--- src/component/App.js
+++ src/component/App.js
@@ -1,34 +1,34 @@
 import {service} from "../service/service";
-import React, {useCallback, useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import './App.css';
 import {Alert, Button, Col, Container, Row, Table} from "react-bootstrap";
 
 function App() {
-    const refContainer = useRef('');
+    const find = useRef('hello')
     const [show, setShow] = useState(false)
     const [org, setOrg] = useState({
         organization: [], find: "hello", error: null
     })
-
-    const onNameChange = (e) => {
-        refContainer.current =e.target.value
-    };
-
-    const fetchOrganization = ()=>{
-        service.getCompany({title: refContainer.current})
+    const fetchOrganization = () => {
+         service.getOrganizations(find.current)
             .then(res => setOrg({...org, organization: res}))
             .catch(e => setOrg({...org, error: e}))
+        console.log(org)
     }
+
+    const onNameChange = (e) => {
+        find.current = e.target.value
+    };
 
 
     useEffect(() => {
         fetchOrganization()
-        console.log("RENDER1")
-    }, [refContainer.current])
+    }, [find.current])
 
     useEffect(() => {
-        if (org.error !== null) setShow(true)
-        console.log("RENDER2")
+        if (org.error !== null) {
+            setShow(true)
+        }
     }, [org.error])
 
     const closeModal = () => {
@@ -36,9 +36,10 @@ function App() {
         setOrg({...org, error: null})
     }
 
-    return (
-        <div className="container">
+    return (<div className="container">
         <header className="App-header">
+
+
             <>
                 <Alert show={show} variant="success" style={{width: "100%"}}>
                     <Alert.Heading>Error!</Alert.Heading>
@@ -53,7 +54,9 @@ function App() {
                     </div>
                 </Alert>
             </>
-            <Container className="m-5 row">
+
+
+           <Container className="m-5 row">
                <Row>
                    <Col sm={8}>
                        <input type="text" onChange={onNameChange} className="input-group h-100"/>
@@ -61,30 +64,36 @@ function App() {
                    <Col sm={4}>
                        <button onClick={fetchOrganization} className="btn btn-outline-secondary">find organisation</button>
                    </Col>
+
+
+
                </Row>
            </Container>
-            {org.organization !== [null] ? org.organization.map((organ, index) => {
-                const {title, content, done, priority, avatar_url, id} = organ
+            {org.organization ? org.organization.map((organ, index) => {
+                const {name, watchers, forks, open_issues, owner, id} = organ
                 return (
+
                     <Table striped bordered hover key={id} >
                         <thead>
                         <tr>
                             <th>#</th>
                             <th>Avatar</th>
-                            <th>Title</th>
-                            <th>Content</th>
-                            <th>Priority</th>
-                            <th>Done</th>
+                            <th>Name Organization</th>
+                            <th>Watchers</th>
+                            <th>Forks</th>
+                            <th>open_issues</th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
+                            {/*<li key={id}>*/}
                             <td>{index + 1}</td>
-                            <td><img src={avatar_url} width="80px" height="80px"/></td>
-                            <td>{title}</td>
-                            <td>{content}</td>
-                            <td>{priority}</td>
-                            <td>{done.toString()}</td>
+                            <td><img src={owner.avatar_url} width="80px" height="80px"/></td>
+                            <td>{name}</td>
+                            <td>{watchers}</td>
+                            <td>{forks}</td>
+                            <td>{open_issues}</td>
+                            {/*</li>*/}
                         </tr>
                         </tbody>
                     </Table>)
